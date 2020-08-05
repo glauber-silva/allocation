ced8f9d17545b534d748e52c9553252b4ed86a39
Glauber Silva <glauber.silva@lendico.com.br>
2020-08-05 00:38:09 -0300

    - Add test for sqlalchemy repository - Add test for orderline model

--- tests/application/adapters/sql_alchemy_batch_repository/test_add.py
+++ tests/application/adapters/sql_alchemy_batch_repository/test_add.py
@@ -1,25 +1,13 @@
-def test_orderline_mapper_can_load_lines(session):
-
-    session.execute(
-        f'INSERT INTO order_Lines (order_id, sku, qty) VALUES'
-        f'("ORDER-1", "RED-CHAIR", 12),'
-        f'("ORDER-1", "RED-TABLE", 13),'
-        f'("ORDER-2", "BLUE-LIPSTICK", 14),'
-
-    )
-
-    expected = [
-        model.OrderLine("ORDER-1", "RED-CHAIR", 12),
-        model.OrderLine("ORDER-1", "RED-TABLE", 13),
-        model.OrderLine("ORDER-2", "BLUE-LIPSTICK", 14)
-    ]
-   
-   assert session.query(model.OrderLine.all()) == expected
+from main.domain.batch import Batch
 
 
-def test_orderline_mapper_can_save_line(session):
-    new_line = model.OrderLine("ORDER-1", "DECORATIVE-WIDGET", 12)
-    session.add(new_line)
-    session.commit()
-    rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
-    assert rows == [("ORDER-1", "DECORATIVE-WIDGET", 12)]
+def test_repository_can_save_a_batch(app):
+    batch = Batch(ref="batch1", sku="RUSTY-SOAPDISH", qty=100, eta=None)
+
+    repository = SqlAlchemyBatchRepository()
+    repository.add(batch=batch)
+    
+    rows = list(db.session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches" '
+    ))
+    assert rows == [("batch1", "RUSTY-SOAPDISH", 100, None)]
---
+++ tests/application/models/test_orderline_model.py
@@ -0,0 +1,25 @@
+def test_orderline_mapper_can_load_lines(session):
+
+    session.execute(
+        f'INSERT INTO order_Lines (order_id, sku, qty) VALUES'
+        f'("ORDER-1", "RED-CHAIR", 12),'
+        f'("ORDER-1", "RED-TABLE", 13),'
+        f'("ORDER-2", "BLUE-LIPSTICK", 14),'
+
+    )
+
+    expected = [
+        model.OrderLine("ORDER-1", "RED-CHAIR", 12),
+        model.OrderLine("ORDER-1", "RED-TABLE", 13),
+        model.OrderLine("ORDER-2", "BLUE-LIPSTICK", 14)
+    ]
+   
+   assert session.query(model.OrderLine.all()) == expected
+
+
+def test_orderline_mapper_can_save_line(session):
+    new_line = model.OrderLine("ORDER-1", "DECORATIVE-WIDGET", 12)
+    session.add(new_line)
+    session.commit()
+    rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
+    assert rows == [("ORDER-1", "DECORATIVE-WIDGET", 12)]
